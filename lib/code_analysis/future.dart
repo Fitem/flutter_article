7d93dad47bc0830a9d5459fdd452a30a55f3c390
leiguangwu <guangwu.lei@genlot.com>
2023-03-08 10:49:46 +0800

    feat: 完成Future测试用例

--- lib/code_analysis/future.dart
+++ lib/code_analysis/future.dart
@@ -19,7 +19,19 @@ void main() {
   // futureCatchErrorTest();
 
   // onError
-  futureOnErrorTest();
+  // futureOnErrorTest();
+
+  // Future.whenComplete
+  // futureWhenCompleteTest();
+
+  // Future.wait
+  // futureWaitTest();
+
+  // Future.timeout
+  // futureTimeoutTest();
+
+  /// Future队列
+  futureQueueTest();
 }
 
 int _count = 0;
@@ -85,44 +97,74 @@ void futureOnErrorTest() {
   print('1.开始count=$_count');
   Future(() {
     _count++;
-    // throw Exception('计算错误1');
+    throw Exception('计算错误1');
   }).then((value) {
     print('2.计算完成count=$_count');
-    // throw Exception('计算错误2');
-  }).then((value){
     throw Exception('计算错误2');
-  },  onError: (error) {
-    print('3.捕获异常 : $error');
+  }).then((value) {
     throw Exception('计算错误3');
-  }).catchError((error) {
-    print('4.捕获异常$error');
+  }, onError: (error) {
+    print('3.捕获异常 : $error');
     throw Exception('计算错误4');
-  })
-  //     .catchError((error) {
-  //   print('5.捕获异常$error');
-  // })
-      //     .then((value) {
-      //   throw Exception('计算错误2');
-      // }).catchError((error) {
-      //   print('5.捕获异常$error');
-      //   throw Exception(error);
-      // })
-      //     .then((value) => print('6.计算完成count=$_count'), onError: (error) {
-      //   print('7.捕获异常 : $error');
-      // })
-      ;
+  }).catchError((error) {
+    print('4.捕获异常 : $error');
+    throw error;
+  }).then((value) {
+    // throw Exception('计算错误4');
+  }, onError: (error) {
+    print('5.捕获异常 : $error');
+    // throw Exception('计算错误5');
+  });
 }
 
+/// Future.whenComplete
+void futureWhenCompleteTest() {
+  Future(() {
+    throw '发生错误';
+  }).then(print).catchError(print).whenComplete(() => print('whenComplete'));
+}
+
+/// Future.wait
+void futureWaitTest() {
+  Future.wait([
+    Future(() => print('A')),
+    Future(() => print('B')),
+    Future(() => print('C')),
+  ]).then((value) => print('D\n'));
+
+  Future.wait(
+    [
+      Future(() => print('A')),
+      Future(() => throw Exception('任务B异常')),
+      Future(() => print('C')),
+    ],
+  ).then((value) => print('D')).catchError(print);
+}
+
+/// Future.timeout
+void futureTimeoutTest() {
+  Future(() => Future.delayed(Duration(seconds: 3), () => print('A')))
+      .timeout(Duration(seconds: 2), onTimeout: () => print('超时'))
+      .then((value) => print('B'));
+}
+
+/// Dart的事件循环 - event loop
 void futureQueueTest() {
   Future future1 = Future(() => null);
   future1.then((value) {
+    // 3.打印6
     print('6');
+    // 5.打印7
     scheduleMicrotask(() => print('7'));
-  }).then((value) => print('8'));
-  Future future2 = Future(() => print('1'));
-  Future(() => print('2'));
-  scheduleMicrotask(() => print('3'));
-  future2.then((value) => print('4'));
+  }).then((value) => print('8')); // 4.打印8
 
+  Future future2 = Future(() => print('1')); // 6.打印1
+  // 8.打印2
+  Future(() => print('2'));
+  // 2.打印3
+  scheduleMicrotask(() => print('3'));
+  // 7.打印4
+  future2.then((value) => print('4'));
+  // 1.打印5
   print('5');
 }
