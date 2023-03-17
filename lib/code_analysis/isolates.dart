e24c6e3df2415fb0cdbb3d1a0cf4f338e4826821
leiguangwu <guangwu.lei@genlot.com>
2023-03-17 13:45:10 +0800

    feat: 调整isolate用例代码

--- lib/code_analysis/isolates.dart
+++ lib/code_analysis/isolates.dart
@@ -10,8 +10,12 @@ Future<void> main() async {
   // isolateTest1();
   /// 1.2 Isolate的异常处理
   isolateTest2();
-
+  /// 1.3 Isolate.run()
   // isolateTest3();
+  /// 2.1 compute()
+  // isolateTest4();
+  /// 2.2 compute()的异常处理
+  // isolateTest5();
 }
 
 /// 1.1 Isolate的基本用法
@@ -21,6 +25,7 @@ Future<void> isolateTest1() async {
   await Isolate.spawn(readAndParseJson, [resultPort.sendPort, downloadLink]);
   String fileContent = await resultPort.first as String;
   print('展示文件内容: $fileContent');
+  resultPort.close();
 }
 
 /// 1.2 Isolate的异常处理
@@ -37,24 +42,44 @@ Future<void> isolateTest2() async {
 
   // 获取结果
   final response = await resultPort.first;
-  if (response == null) { // 没有消息
+  resultPort.close();
+  if (response == null) {
+    // 没有消息
     print('没有消息');
-  } else if (response is List) { // 异常消息
+  } else if (response is List) {
+    // 异常消息
     final errorAsString = response[0]; //异常
     final stackTraceAsString = response[1]; // 堆栈信息
     print('error: $errorAsString \nstackTrace: $stackTraceAsString');
-  } else { // 正常消息
+  } else {
+    // 正常消息
     print(response);
   }
 }
 
-/// 3. compute
+/// 1.3 Isolate.run()
 Future<void> isolateTest3() async {
+  const String downloadLink = '下载链接';
+  String fileContent = await Isolate.run(() => handleReadAndParseJson(downloadLink));
+  print('展示文件内容: $fileContent');
+}
+
+/// 2.1 compute()
+Future<void> isolateTest4() async {
+  // String content = await compute((link) async {
+  //   print('开始下载: $link');
+  //   await Future.delayed(const Duration(seconds: 2));
+  //   return '下载的内容';
+  // }, '下载链接');
+  // print('完成下载: $content');
+}
+
+/// 2.2 compute()的异常处理
+Future<void> isolateTest5() async {
   // try {
-  //   String newLink = await compute((link) async {
+  //   await compute((link) async {
   //     await Future.delayed(const Duration(seconds: 2));
   //     throw Exception('下载失败');
-  //     return '新的链接';
   //   }, '下载链接');
   // } catch (e) {
   //   print('error: $e');
@@ -67,11 +92,17 @@ Future<void> readAndParseJson(List<dynamic> args) async {
   SendPort resultPort = args[0];
   String fileLink = args[1];
 
-  print('获取下载链接: $fileLink');
+  String fileContent = await handleReadAndParseJson(fileLink);
 
+  Isolate.exit(resultPort, fileContent);
+}
+
+/// 处理读取并解析文件内容
+Future<String> handleReadAndParseJson(String fileLink) async {
+  print('获取下载链接: $fileLink');
   String fileContent = '文件内容';
   await Future.delayed(const Duration(seconds: 2));
-  Isolate.exit(resultPort, fileContent);
+  return fileContent;
 }
 
 /// 读取并解析文件内容, 上报异常
